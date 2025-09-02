9b067c8f899feee15234835676c4aa9416ea5b9d
samthevibe <freejack1984@gmail.com>
2025-09-02 16:34:07 -0400

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 
-// Temporary data storage
 let singles = [
   { id: 1, name: "Hit Single 1", streams: 0 },
   { id: 2, name: "Hit Single 2", streams: 0 }
@@ -13,32 +12,23 @@ let albums = [
   { id: 2, name: "Album 2", streams: 0 }
 ];
 
-// Increment single streams
-router.post('/single/:id/stream', (req, res) => {
-  const single = singles.find(s => s.id == req.params.id);
-  if (!single) return res.status(404).send("Single not found");
-  single.streams++;
-  res.send(single);
+// Home page route
+router.get('/', (req, res) => {
+  let html = `
+    <h1>JUKE Music App</h1>
+    <h2>Singles</h2>
+    <ul>
+      ${singles.map(s => `<li>${s.name} - Streams: ${s.streams}</li>`).join('')}
+    </ul>
+    <h2>Albums</h2>
+    <ul>
+      ${albums.map(a => `<li>${a.name} - Streams: ${a.streams}</li>`).join('')}
+    </ul>
+  `;
+  res.send(html);
 });
 
-// Increment album streams
-router.post('/album/:id/stream', (req, res) => {
-  const album = albums.find(a => a.id == req.params.id);
-  if (!album) return res.status(404).send("Album not found");
-  
-  // 12 single streams = 1 album stream
-  album.streams++;
-  res.send(album);
-});
+// ...other routes like /singles, /albums, POST streams
 
-// Get all singles
-router.get('/singles', (req, res) => {
-  res.json(singles);
-});
+module.exports = { router, singles, albums };
 
-// Get all albums
-router.get('/albums', (req, res) => {
-  res.json(albums);
-});
-
-module.exports = router;
