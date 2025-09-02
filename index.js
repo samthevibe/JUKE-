488424ed39c2a087571e4dae03fc4f7b16403332
samthevibe <freejack1984@gmail.com>
2025-09-02 17:09:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// index.js
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT||3000;
 
+app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 const { router } = require('./routes');
 app.use('/', router);
 
-app.listen(PORT, () => console.log(`JUKE running on port ${PORT}`));
+app.listen(PORT,()=>console.log(`JUKE running on port ${PORT}`));
