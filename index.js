efbfe79a8399acf87feb2255697fa08b8251d70a
samthevibe <freejack1984@gmail.com>
2025-09-02 16:29:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ app.use(express.json());
 const routes = require('./routes');
 app.use('/', routes);
 
+// New home route
+app.get('/', (req, res) => {
+  // Get singles and albums from routes.js
+  const { singles, albums } = require('./routes');
+
+  let html = `
+    <h1>JUKE Music App</h1>
+    <h2>Singles</h2>
+    <ul>
+      ${singles.map(s => `<li>${s.name} - Streams: ${s.streams}</li>`).join('')}
+    </ul>
+    <h2>Albums</h2>
+    <ul>
+      ${albums.map(a => `<li>${a.name} - Streams: ${a.streams}</li>`).join('')}
+    </ul>
+  `;
+  res.send(html);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
