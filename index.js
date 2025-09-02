7ed398d8e8ab7da137237f2615c633743bf009d9
samthevibe <freejack1984@gmail.com>
2025-09-02 16:36:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
+// index.js
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// JSON parsing
 app.use(express.json());
 
+// Import routes
 const { router } = require('./routes');
 app.use('/', router);
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
