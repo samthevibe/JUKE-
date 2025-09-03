612b44e76bf63e50cf2e42b28f5cb7b81693594b
samthevibe <freejack1984@gmail.com>
2025-09-03 10:21:00 -0400

    Create routes.js

---
+++ Backend/routes.js
@@ -0,0 +1,6 @@
+// Currently empty, but you can move routes from index.js here later
+// Example:
+// const express = require('express');
+// const router = express.Router();
+// router.get('/albums', ...);
+// module.exports = router;
